7e56a2704315eefcf60d15797496817c05b6c452
johnshumiston <johnshumiston@gmail.com>
2016-03-30 22:21:34 -0400

    big updates

--- guessingGame.js
+++ guessingGame.js
@@ -1,48 +1,82 @@
-/* **** Global Variables **** */
-// try to elminate these global variables in your project, these are here just to start.
 $(document).ready(function() {
+// Generate the Winning Number
 
-
+var totalGuesses =[];
 var playersGuess
 var winningNumber
-
-
-
-/* **** Guessing Game Functions **** */
-
-// Generate the Winning Number
 
 function generateWinningNumber(){
 	winningNumber = Math.floor(Math.random()*100)+1;
 	return winningNumber;
-}
-
+};
+var winningNumber=generateWinningNumber();
 // Fetch the Players Guess
 
 function playersGuessSubmission(){
-	playersGuess = +$('#guess').val();
-	$('#guess').append('playersGuess');
-	$('#guess').val("");
+	playersGuess = +$('#input').val();
+	$('#input').append('playersGuess');
+	$('#input').val("");
 	$('playersGuess').remove();
+	$('#status').text("").css('text-align', 'center');
+	checkGuess(playersGuess, winningNumber);
+	if(totalGuesses.length<4){
+		$('#remainingguesses').text((5-totalGuesses.length) + " guesses remain...!");
+	}
+	if(totalGuesses.length===4){
+		$('#remainingguesses').text((5-totalGuesses.length) + " guess remains...!");
+	}
+	lowerOrHigher(playersGuess, winningNumber);
 };
+
 // Determine if the next guess should be a lower or higher number
-$('#theguess').on('click', playersGuessSubmission);
 
-
-function lowerOrHigher(){
-	// add code here
-}
+function lowerOrHigher(playersGuess, winningNumber){
+	if(playersGuess===winningNumber){
+		$('#status').text("You win!!!").css('text-align','center');
+	}
+	else if(playersGuess<winningNumber){
+		$('#status').append("Maybe... aim higher..." + "You're like " + (Math.round(Math.abs(winningNumber-playersGuess) / 10) * 10) + "ish off.").css('text-align','center');
+	}
+	else if(playersGuess>winningNumber){
+		$('#status').append("Er... aim lower..." + "You're like " + (Math.round(Math.abs(winningNumber-playersGuess) / 10) * 10) + "ish off.").css('text-align','center');
+	}
+};
 
 // Check if the Player's Guess is the winning number 
 
-function checkGuess(){
-	// add code here
-}
+function checkGuess(playersGuess, winningNumber){
+		for(i=0; i<totalGuesses.length; i++) {
+			if(totalGuesses[i]===playersGuess){
+				$('#status').text("This is a duplicate").css('text-align','center');
+			}
+		};
+		if(playersGuess===winningNumber){
+			$('#status').text("You win!!!").css('text-align','center');
+		}
+		if(playersGuess!==winningNumber){
+			$('#status').text("Try again...").css('text-align','center');
+			totalGuesses.push(playersGuess);
+		}
+	};
 
 // Create a provide hint button that provides additional clues to the "Player"
 
 function provideHint(){
-	// add code here
+	if(totalGuesses.length===0){
+		$('#status').text((Math.floor(Math.random()*100)+1) + ", " + (Math.floor(Math.random()*100)+1) + ", " + (Math.floor(Math.random()*100)+1) + ", " + (Math.floor(Math.random()*100)+1) + ", " + (Math.floor(Math.random()*100)+1) + ", " + (Math.floor(Math.random()*100)+1) + ", " + (Math.floor(Math.random()*100)+1) + ", " + winningNumber + "...one of these is it!");
+	}
+	if(totalGuesses.length===1){
+		$('#status').text((Math.floor(Math.random()*100)+1) + ", " + (Math.floor(Math.random()*100)+1) + ", " + (Math.floor(Math.random()*100)+1) + ", " + winningNumber + ", " + (Math.floor(Math.random()*100)+1) + ", " + (Math.floor(Math.random()*100)+1) + ", " + (Math.floor(Math.random()*100)+1) + "...one of these is it!");
+	}
+	if(totalGuesses.length===2){
+		$('#status').text((Math.floor(Math.random()*100)+1) + ", " + (Math.floor(Math.random()*100)+1) + ", " + winningNumber + ", " + (Math.floor(Math.random()*100)+1) + ", " + (Math.floor(Math.random()*100)+1) + ", " + (Math.floor(Math.random()*100)+1) + "...one of these is it!");
+	}
+	if(totalGuesses.length===3){
+		$('#status').text((Math.floor(Math.random()*100)+1) + ", " + (Math.floor(Math.random()*100)+1) + ", " + (Math.floor(Math.random()*100)+1) + ", " + winningNumber + ", " + (Math.floor(Math.random()*100)+1) + "...one of these is it!");
+	}
+	if(totalGuesses.length===4){
+		$('#status').text((Math.floor(Math.random()*100)+1) + ", " + winningNumber + ", " + (Math.floor(Math.random()*100)+1) + ", " + (Math.floor(Math.random()*100)+1) + "...one of these is it!");
+	}
 }
 
 // Allow the "Player" to Play Again
@@ -50,6 +84,9 @@ function provideHint(){
 function playAgain(){
 	// add code here
 }
+
+$('#submit').on('click', playersGuessSubmission);
+$('#hint').on('click', provideHint);
 
 });
 /* **** Event Listeners/Handlers ****  */
